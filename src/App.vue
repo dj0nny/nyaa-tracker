0b54a6b06950af2ef5d1347f8bfbd1c9d1a2e4a3
dj0nny <frank96990@gmail.com>
2023-01-14 15:53:24 +0100

    add Navbar and router

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
-<template>hello world</template>
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import AppNavbar from './components/AppNavbar.vue';
+</script>
+
+<template>
+  <AppNavbar />
+  <main class="container mx-auto my-10">
+    <RouterView />
+  </main>
+</template>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header
+    class="navbar bg-primary text-primary-content flex justify-center items-center py-4"
+  >
+    <RouterLink
+      :to="{ name: 'home' }"
+      class="btn btn-ghost normal-case text-3xl"
+    >
+      Nyaa Tracker
+    </RouterLink>
+  </header>
+</template>
